bc4229eea739458b087e31826c821ceb65816bbb
eug2ne <rebor2dlivekirax2@gmail.com>
2021-08-11 21:23:53 +0900

    지원
    게임에 필요한 함수 개발 및 그 과정에서 html 및 css 파일 내용 일부 수정

--- prototype/game.js
+++ prototype/game.js
@@ -1,14 +1,44 @@
-function letterChoose() {
-    let l_array = {};
+function letterChoose(target, option) {
+    // return array of choosable letters for each option
+    const table = target.closest("table");
+    const x_pos = parseInt(target.ariaColIndex);
+    const y_pos = parseInt(target.ariaRowIndex);
 
-    const letter = document.querySelector("td");
-    letter.addEventListener("click", e => {
-        l_array[e.target] = e.target.innerHTML;
-    })
+    if (option == "merge")
+        () => {
+            if (target.ariaColIndex == "0")
+                return [ target, table.querySelector(`td[aria-colindex="1"]`) ];
+
+            else if (target.ariaColIndex == "14")
+                return [ table.querySelector(`td[aria-colindex="13"]`), target ];
+
+            else
+                return [ table.querySelector(`td[aria-colindex="${x_pos-1}"]`), target, table.querySelector(`td[aria-colindex="${x_pos+1}"]`) ]  
+        }
+
+    else 
+        () => {
+            if (target.ariaRowIndex in ["5", "4", "3"])
+                return [ table.querySelector(`td[aria-rowindex="3"][aria-colindex="${x_pos}"]`),
+                table.querySelector(`td[aria-rowindex="3"][aria-colindex="${x_pos+1}"]`),
+                table.querySelector(`td[aria-rowindex="4"][aria-colindex="${x_pos}"]`),
+                table.querySelector(`td[aria-rowindex="4"][aria-colindex="${x_pos+1}"]`),
+                table.querySelector(`td[aria-rowindex="5"][aria-colindex="${x_pos}"]`),
+                table.querySelector(`td[aria-rowindex="5"][aria-colindex="${x_pos+1}"]`) ];
+
+            else
+                return [ table.querySelector(`td[aria-rowindex="${y_pos}"][aria-colindex="${x_pos}"]`),
+                table.querySelector(`td[aria-rowindex="${y_pos}"][aria-colindex="${x_pos+1}"]`),
+                table.querySelector(`td[aria-rowindex="${y_pos+1}"][aria-colindex="${x_pos}"]`),
+                table.querySelector(`td[aria-rowindex="${y_pos+1}"][aria-colindex="${x_pos+1}"]`),
+                table.querySelector(`td[aria-rowindex="${y_pos+2}"][aria-colindex="${x_pos}"]`),
+                table.querySelector(`td[aria-rowindex="${y_pos+2}"][aria-colindex="${x_pos+1}"]`) ];
+        }
 }
 
-function merge() {
-    // merge selected letters to right
+function merge(target) {
+    // merge selected letters to left
+
 }
 
 function word() {
--- prototype/main.js
+++ prototype/main.js
@@ -1,26 +1,23 @@
+// reload function
 document.addEventListener("click", e => {
     if (e.target.matches("h1"))
         location.reload();
 })
 
-function options(target) {
-    let options = document.createElement("div");
+// options-window function
+function options_window(e) {
+    const {clientX: mouseX, clientY: mouseY} = e;
+    const options_window = document.querySelector("#options-window");
 
-    const merge = document.createElement("div");
-    merge.innerHTML = "합치기";
-    
-    const word = document.createElement("div");
-    word.innerHTML = "단어 조합";
-    
-    const blank = document.createElement("div");
-    blank.innerHTML = "빈칸";
+    options_window.style.top = `${mouseY}px`;
+    options_window.style.left = `${mouseX}px`;
 
-    options.append([merge, word, blank]);
-    options.position = target.position;
-    options.style.getPropertyValue("options.css");
+    options_window.classList.add("visible");
+
+    return e.target;
 }
 
-document.addEventListener("click", e => {
+document.body.addEventListener("click", e => {
     if (e.target.matches("td"))
-        options.call({}, e.target);
-})
+        options_window(e);
+});

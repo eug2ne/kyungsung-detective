929602d337c958fecd0193b1cb60b7d5f906d15e
eug2ne <rebor2dlivekirax2@gmail.com>
2021-08-14 17:30:29 +0900

    commit

--- prototype/game.js
+++ prototype/game.js
@@ -7,37 +7,60 @@ function letterChoose(target, option) {
     if (option == "merge")
         () => {
             if (target.ariaColIndex == "0")
-                return [ target, table.querySelector(`td[aria-colindex="1"]`) ];
+                table.querySelector(`td[aria-colindex="1"]`).classList.add("choosable");
 
             else if (target.ariaColIndex == "14")
-                return [ table.querySelector(`td[aria-colindex="13"]`), target ];
+                table.querySelector(`td[aria-colindex="13"]`).classList.add("choosable");
 
             else
-                return [ table.querySelector(`td[aria-colindex="${x_pos-1}"]`), target, table.querySelector(`td[aria-colindex="${x_pos+1}"]`) ]  
+                for (let x in [ table.querySelector(`td[aria-colindex="${x_pos-1}"]`), table.querySelector(`td[aria-colindex="${x_pos+1}"]`) ]) {
+                    x.classList.add("choosable");
+                }
         }
 
     else 
         () => {
             if (target.ariaRowIndex in ["5", "4", "3"])
-                return [ table.querySelector(`td[aria-rowindex="3"][aria-colindex="${x_pos}"]`),
-                table.querySelector(`td[aria-rowindex="3"][aria-colindex="${x_pos+1}"]`),
-                table.querySelector(`td[aria-rowindex="4"][aria-colindex="${x_pos}"]`),
-                table.querySelector(`td[aria-rowindex="4"][aria-colindex="${x_pos+1}"]`),
-                table.querySelector(`td[aria-rowindex="5"][aria-colindex="${x_pos}"]`),
-                table.querySelector(`td[aria-rowindex="5"][aria-colindex="${x_pos+1}"]`) ];
+                () => {
+                    for (let x in [ table.querySelector(`td[aria-rowindex="3"][aria-colindex="${x_pos}"]`),
+                    table.querySelector(`td[aria-rowindex="3"][aria-colindex="${x_pos+1}"]`),
+                    table.querySelector(`td[aria-rowindex="4"][aria-colindex="${x_pos}"]`),
+                    table.querySelector(`td[aria-rowindex="4"][aria-colindex="${x_pos+1}"]`),
+                    table.querySelector(`td[aria-rowindex="5"][aria-colindex="${x_pos}"]`),
+                    table.querySelector(`td[aria-rowindex="5"][aria-colindex="${x_pos+1}"]`) ]) {
+                        x.classList.add("choosable");
+                    }
+                }
+
+            else if (target.ariaRowIndex in ["0", "1", "2"])
+                () => {
+                    for (let x in [ table.querySelector(`td[aria-rowindex="0"][aria-colindex="${x_pos}"]`),
+                    table.querySelector(`td[aria-rowindex="0"][aria-colindex="${x_pos+1}"]`),
+                    table.querySelector(`td[aria-rowindex="1"][aria-colindex="${x_pos}"]`),
+                    table.querySelector(`td[aria-rowindex="1"][aria-colindex="${x_pos+1}"]`),
+                    table.querySelector(`td[aria-rowindex="2"][aria-colindex="${x_pos}"]`),
+                    table.querySelector(`td[aria-rowindex="2"][aria-colindex="${x_pos+1}"]`) ]) {
+                        x.classList.add("choosable");
+                    }
+                }
 
             else
-                return [ table.querySelector(`td[aria-rowindex="${y_pos}"][aria-colindex="${x_pos}"]`),
-                table.querySelector(`td[aria-rowindex="${y_pos}"][aria-colindex="${x_pos+1}"]`),
-                table.querySelector(`td[aria-rowindex="${y_pos+1}"][aria-colindex="${x_pos}"]`),
-                table.querySelector(`td[aria-rowindex="${y_pos+1}"][aria-colindex="${x_pos+1}"]`),
-                table.querySelector(`td[aria-rowindex="${y_pos+2}"][aria-colindex="${x_pos}"]`),
-                table.querySelector(`td[aria-rowindex="${y_pos+2}"][aria-colindex="${x_pos+1}"]`) ];
+                () => {
+                    for (let x in [ table.querySelector(`td[aria-rowindex="${y_pos}"][aria-colindex="${x_pos}"]`),
+                    table.querySelector(`td[aria-rowindex="${y_pos}"][aria-colindex="${x_pos+1}"]`),
+                    table.querySelector(`td[aria-rowindex="${y_pos+1}"][aria-colindex="${x_pos}"]`),
+                    table.querySelector(`td[aria-rowindex="${y_pos+1}"][aria-colindex="${x_pos+1}"]`),
+                    table.querySelector(`td[aria-rowindex="${y_pos+2}"][aria-colindex="${x_pos}"]`),
+                    table.querySelector(`td[aria-rowindex="${y_pos+2}"][aria-colindex="${x_pos+1}"]`) ]) {
+                        x.classList.add("choosable");
+                    }
+                }
         }
 }
 
 function merge(target) {
     // merge selected letters to left
+<<<<<<< Updated upstream
     const pre_array = letterChoose(target, "merge");
 
     for (let pre_letter in pre_array) {
@@ -46,15 +69,33 @@ function merge(target) {
             // if (e.target.innerHTML in possible_merge[`${target.innerHTML}`])
                 
         // })
+=======
+    letterChoose(target, "merge");
+    let choosable_array = document.querySelectorAll(`td.choosable`);
+
+    for (let choosable in choosable_array) {
+        choosable.addEventListener("click", e => {
+            if (e.target.innerHTML in valid_mergewordlist.valid[`${target.innerHTML}`])
+                () => {
+                target.innerHTML = valid_mergewordlist.merge[`${target.innerHTML},${e.target.innerHTML}`];
+                }
+            // else
+                // error("InvalidMergeError")
+        })
+>>>>>>> Stashed changes
     }
 }
 
-function word() {
+function word(target) {
     // l_array : array of selected letters
     // make word out of selected letters
     // if impossible, WordImpossibleError
+    letterChoose(target, "word");
+    let choosable_array = document.querySelectorAll(`td.choosable`);
 }
 
 function blanck() {
     // if WordImpossibleError + option clicked, make blank space
+    letterChoose(target, "word");
+    let choosable_array = document.querySelectorAll(`td.choosable`);
 }
--- prototype/main.js
+++ prototype/main.js
@@ -13,11 +13,26 @@ function options_window(e) {
     options_window.style.left = `${mouseX}px`;
 
     options_window.classList.add("visible");
-
-    return e.target;
 }
 
 document.body.addEventListener("click", e => {
     if (e.target.matches("td"))
         options_window(e);
 });
+
+// target obj
+    // target obj exists only one at a time
+    // if new element is set to target, update target obj
+var target = {
+    "pos" : "null",
+    "letter" : "null",
+    "merge" : function merge() {
+        // merge function
+    },
+    "word" : function word() {
+        // word function
+    },
+    "blank" : function blank() {
+        // blank function
+    }
+}

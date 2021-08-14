0919cfa76f895f3fde2d7d7d016cb3ae561e4fe8
eug2ne <rebor2dlivekirax2@gmail.com>
2021-08-15 00:51:46 +0900

    commit

--- prototype/main.js
+++ prototype/main.js
@@ -21,6 +21,68 @@ document.body.addEventListener("click", e => {
         options_window(e);
 });
 
+// letterChoose function
+    // return choosable_array for each method
+    function letterChoose(target, option) {
+        // return array of choosable letters for each option
+        const table = target.closest("table");
+        const x_pos = parseInt(target.ariaColIndex);
+        const y_pos = parseInt(target.ariaRowIndex);
+    
+        if (option == "merge")
+            () => {
+                if (target.ariaColIndex == "0")
+                    table.querySelector(`td[aria-colindex="1"]`).classList.add("choosable");
+    
+                else if (target.ariaColIndex == "14")
+                    table.querySelector(`td[aria-colindex="13"]`).classList.add("choosable");
+    
+                else
+                    for (let x in [ table.querySelector(`td[aria-colindex="${x_pos-1}"]`), table.querySelector(`td[aria-colindex="${x_pos+1}"]`) ]) {
+                        x.classList.add("choosable");
+                    }
+            }
+    
+        else 
+            () => {
+                if (target.ariaRowIndex in ["5", "4", "3"])
+                    () => {
+                        for (let x in [ table.querySelector(`td[aria-rowindex="3"][aria-colindex="${x_pos}"]`),
+                        table.querySelector(`td[aria-rowindex="3"][aria-colindex="${x_pos+1}"]`),
+                        table.querySelector(`td[aria-rowindex="4"][aria-colindex="${x_pos}"]`),
+                        table.querySelector(`td[aria-rowindex="4"][aria-colindex="${x_pos+1}"]`),
+                        table.querySelector(`td[aria-rowindex="5"][aria-colindex="${x_pos}"]`),
+                        table.querySelector(`td[aria-rowindex="5"][aria-colindex="${x_pos+1}"]`) ]) {
+                            x.classList.add("choosable");
+                        }
+                    }
+    
+                else if (target.ariaRowIndex in ["0", "1", "2"])
+                    () => {
+                        for (let x in [ table.querySelector(`td[aria-rowindex="0"][aria-colindex="${x_pos}"]`),
+                        table.querySelector(`td[aria-rowindex="0"][aria-colindex="${x_pos+1}"]`),
+                        table.querySelector(`td[aria-rowindex="1"][aria-colindex="${x_pos}"]`),
+                        table.querySelector(`td[aria-rowindex="1"][aria-colindex="${x_pos+1}"]`),
+                        table.querySelector(`td[aria-rowindex="2"][aria-colindex="${x_pos}"]`),
+                        table.querySelector(`td[aria-rowindex="2"][aria-colindex="${x_pos+1}"]`) ]) {
+                            x.classList.add("choosable");
+                        }
+                    }
+    
+                else
+                    () => {
+                        for (let x in [ table.querySelector(`td[aria-rowindex="${y_pos}"][aria-colindex="${x_pos}"]`),
+                        table.querySelector(`td[aria-rowindex="${y_pos}"][aria-colindex="${x_pos+1}"]`),
+                        table.querySelector(`td[aria-rowindex="${y_pos+1}"][aria-colindex="${x_pos}"]`),
+                        table.querySelector(`td[aria-rowindex="${y_pos+1}"][aria-colindex="${x_pos+1}"]`),
+                        table.querySelector(`td[aria-rowindex="${y_pos+2}"][aria-colindex="${x_pos}"]`),
+                        table.querySelector(`td[aria-rowindex="${y_pos+2}"][aria-colindex="${x_pos+1}"]`) ]) {
+                            x.classList.add("choosable");
+                        }
+                    }
+            }
+    }
+
 // target obj
     // if new td clicked >> update target obj
 let target = {

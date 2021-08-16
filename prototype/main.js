4280d7b5345d141cef03ba04fb942ce87cb4b2f6
eug2ne <rebor2dlivekirax2@gmail.com>
2021-08-16 11:38:48 +0900

    commit08.16

--- prototype/main.js
+++ prototype/main.js
@@ -1,148 +1,92 @@
-// reload function
-document.addEventListener("click", e => {
-    if (e.target.matches("h1"))
-        location.reload();
-})
-
 // options-window function
 function options_window(e) {
-    const {clientX: mouseX, clientY: mouseY} = e;
+    const { clientX: mouseX, clientY: mouseY } = e;
     const options_window = document.querySelector("#options-window");
 
     options_window.style.top = `${mouseY}px`;
     options_window.style.left = `${mouseX}px`;
 
-    options_window.style.display="block";
+    options_window.style.display = "block";
     options_window.classList.add("visible");
 }
 
-document.body.addEventListener("click", e => {
+// show up option-window when click on td
+document.addEventListener("click", e => {
     if (e.target.matches("td"))
+        // open up options-window
         options_window(e);
+
+    else {
+        // remove options-window
+        document.querySelector("#options-window").classList.remove("visible");
+        document.querySelector("#options-window").style.display = "none";
+    }
 });
 
-// letterChoose function
-    // return choosable_array for each method
-function letterChoose(target, table, method) {
-    const x_pos = parseInt(target.ariaColIndex);
-    const y_pos = parseInt(target.ariaRowIndex);
-    let rows = [];
-
-    if (method == "merge")
-        () => {
-        if (target.ariaColIndex == "0")
-            table.querySelector(`td[aria-colindex="1"]`).classList.add("choosable");
-    
-        else if (target.ariaColIndex == "14")
-            table.querySelector(`td[aria-colindex="13"]`).classList.add("choosable");
-    
-        else
-            for (let x in [ table.querySelector(`td[aria-colindex="${x_pos-1}"]`), table.querySelector(`td[aria-colindex="${x_pos+1}"]`) ]) {
-                x.classList.add("choosable");
-            }
-        }
-    
-    else 
-        () => {
-        try {
-            rows = [ table.querySelector(`tr[aria-rowindex=${y_pos}]`), this.table.querySelector(`tr[aria-rowindex=${y_pos+1}]`), this.table.querySelector(`tr[aria-rowindex=${y_pos+2}]`) ];
-        } catch(Error) {
-            rows = [ table.querySelector(`tr[aria-rowindex="3"`), this.table.querySelector(`tr[aria-rowindex="4"`), this.table.querySelector(`tr[aria-rowindex="5"`)];
-        }
-
-        for (const row in rows) {
-            try {
-                for (const x in [ x_pos, x_pos+1 ]) {
-                    row.querySelector(`td[aria-colindex="${x}"]`).classList.add("choosable");
-                }
-            } catch(Error) {
-                for (const x in [ 13, 14 ]) {
-                    row.querySelector(`td[aria-colindex="${x}"]`).classList.add("choosable");
-                }
-            }
-        }
-        }
-}
-
 // target obj
-    // if new td clicked >> update target obj
+// if new td clicked >> update target obj
 let target = {
-    pos : null,
-    letter : null,
-    element : null,
-    table : null,
+    x_pos: null,
+    y_pos: null,
+    letter: null,
+    element: null,
+    table: null,
     set clickOnTd(Td) {
         this.element = Td;
         this.letter = Td.innerHTML;
-        this.pos = `${Td.ariaColIndex},${Td.ariaRowIndex}`;
+        this.x_pos = parseInt(Td.ariaColIndex);
+        this.y_pos = parseInt(Td.ariaRowIndex);
         this.table = Td.closest("table");
 
         this.element.classList.add("target");
     },
-    merge : function() {
-        // merge method
-        letterChoose(this.element, this.table, "merge");
-        
-        document.addEventListener("click", e => {
-            if (e.target.matches("td.choosable"))
-                () => {
-                try {
-                    if (e.target.innerHTML in valid_mergewordlist.valid[this.element.innerHTML])
-                        () => {
-                        this.table.querySelector(`td[aria-colindex="${Math.min(this.element.ariaColIndex, e.target.ariaColIndex)}"]`).innerHTML = valid_mergewordlist.merge[`${this.element.innerHTML},${e.target.innerHTML}`];
-                        this.table.querySelector(`td[aria-colindex="${Math.max(this.element.ariaColIndex, e.target.ariaColIndex)}"]`).innerHTML = "";
-                        
-                        // regression till end of row
-                        let row_letter = this.table.querySelector(`td[aria-colindex="${Math.max(this.element.ariaColIndex, e.target.ariaColIndex)}"]`);
-
-                        if (!(row_letter.nextElementSibling))
-                            () => {}
-                        else
-                            () => {
-                            row_letter.innerHTML = row_letter.nextElementSibling.innerHTML;
-                            row_letter = row_letter.nextElementSibling;
-                            }
-
-                        }
-
-                    else
-                        throw new Error("MergeImpossibleError");
-                } catch(Error) {
-                    // show "MegeImpossibleError" on screen
-                } finally {
-                    e.stopPropagation();
-                }
-                }
-        })
-    },
-    word : function() {
-        // function method
-        const x_pos = parseInt(this.element.ariaColIndex);
-        const y_pos = parseInt(this.element.ariaRowIndex);
-        letterChoose(this.element, this.table, "word");
-
-        let chosen = { "0,0":null, "1,0":null, "0,1":null, "1,1":null, "0,2":null, "1,2":null }
-        document.addEventListener("click", e => {
-            if (e.target.matches("td.choosable"))
-                () => {
-                e.target.style.backgroundcolor = "bisque";
-                chosen[`${e.target.ariaColIndex - x_pos},${e.target.ariaRowIndex - y_pos}`] = e.target.innerHTML;
-                }
-
-            else if (e.target.matches("td.target"))
-                return chosen;
-        })
-    },
-    blank : function() {
-        // blank method
-    }
+    merge: function () {},
+    word: function () {},
+    blank: function () {}
 }
 
+// click event for main page
 document.body.addEventListener("click", e => {
     try {
-        target.element.classList.remove("target");
-    } catch (TypeError) {}
+        // delete every td with class (target, choosable)
+        document.querySelectorAll("td.target, td.choosable").forEach(element => element.classList.remove("target", "choosable"));
+    } catch (TypeError) { }
 
-    if (e.target.matches("td"))
-        target.clickOnTd = e.target;
+    finally {
+        if (e.target.matches("td"))
+            //set target obj
+            target.clickOnTd = e.target;
+
+        // reload function
+        else if (e.target.matches("h1"))
+            location.reload();
+    }
+});
+
+// set click event for option
+document.body.addEventListener("click", e => {
+    if (e.target.parentElement.matches("#options-window")) {
+        // show target td
+        target.element.classList.add("target");
+
+        switch (e.target.id) {
+            case "merge":
+                // show choosable td
+                // event: click >> merge with target
+                target.merge();
+                break;
+
+            case "word":
+                // show choosable td
+                // event: click >> append to chosen_letter
+                // create word div
+                target.word();
+                break;
+
+            case "blank":
+                // evaluate target + choosable
+                target.blank();
+                break;
+        }
+    }
 });

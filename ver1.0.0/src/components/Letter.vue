d273c401dc6bbf1d8bba87e9720ee9e8469e0d2e
eug2ne <rebor2dlivekirax2@gmail.com>
2021-11-27 17:15:10 +0900

    space, merge 완성

--- ver1.0.0/src/components/Letter.vue
+++ ver1.0.0/src/components/Letter.vue
@@ -1,6 +1,6 @@
 <template>
   <td @click="clickonLetter"
-  :class="{ target:isTarget, choice:isChoice, answer:isAnswer }" :aria-rowindex="rowIndex" :aria-colindex="colIndex">
+  :class="{ target:isTarget, choice:isChoice, chosen:isChosen, answer:isAnswer }" :aria-rowindex="rowIndex" :aria-colindex="colIndex">
       {{ letter }}
   </td>
 </template>
@@ -8,64 +8,23 @@
 <script>
 export default {
   name: 'Letter',
-  props: ['rowIndex', 'colIndex', 'letter', 'action'],
-  data() {
-    return {
-      isTarget: false,
-      isChoice: false,
-      isChosen: false,
-      isAnswer: false
-    }
-  },
+  props: ['rowIndex', 'colIndex', 'letter', 'isTarget', 'isChoice', 'isChosen', 'isAnswer'],
   methods: {
     clickonLetter() {
       if (this.isChoice||this.isChosen) {
-        // toggle isChoice/isChosen
-        this.isChosen = !this.isChosen
-        this.isChoice = !this.isChosen
-
-        if (this.isChosen) {
-          this.emitter.emit('ppChoices', {'row':this.rowIndex, 'col':parseInt(this.colIndex), 'letter':this.letter}, 'push')
+        // update chosen
+        if (this.isChoice) {
+          this.$emit('toggleChoice', {'choice':{'row':this.rowIndex, 'col':parseInt(this.colIndex)}, 'action':'push'})
         } else {
-          this.emitter.emit('ppChoice', {'row':this.rowIndex, 'col':parseInt(this.colIndex), 'letter':this.letter}, 'pop')
+          this.$emit('toggleChoice', {'choice':{'row':this.rowIndex, 'col':parseInt(this.colIndex)}, 'action':'pop'})
         }
-      } else if (this.isTarget&&this.action == 'word') {
-        this.emitter.emit('forceWord')
       } else {
-        // toggle isTarget
-        this.isTarget = !this.isTarget
-        this.emitter.emit('toggleShow', this.isTarget)
-
-        if (this.isTarget) {
-          this.emitter.emit('targetUpdate', {'row':this.rowIndex, 'col':parseInt(this.colIndex), 'letter':this.letter})
-        }
+        // toggletarget() || forceword()
+        this.$emit('clickOnLetter', {'row':this.rowIndex, 'col':parseInt(this.colIndex), 'target':this.isTarget})
       }
     }
   },
   mounted() {
-    this.emitter.on('targetUpdate', (data) => {
-      if (this.rowIndex == data.row && this.colIndex == data.col) {
-        this.isTarget = true
-      } else {
-          this.isTarget = false
-          this.isChoice = false
-          this.isChosen = false
-        }
-    }),
-    this.emitter.on('showChoices', (data) => {
-      console.log(data.target)
-      if (data.type === 'merge') {
-        if ((this.rowIndex == data.target.row&&this.colIndex == data.target.col-1)||(this.rowIndex == data.target.row&&this.colIndex == data.target.col+1)) {
-          this.isChoice = true
-        }
-      } else if (data.type === 'word') {
-        if ((this.rowIndex == data.taret.row&&this.colIndex == data.target.col+1)||((data.target.row < this.rowIndex&&this.rowIndex < data.target.row+3)&&(data.target.col-1 < this.colIndex&&this.colIndex < data.target.col+2))) {
-          this.isChoice = true
-        }
-      } else {
-        this.emitter.emit('space')
-      }
-    })
   }
 }
 </script>
--- ver1.0.0/src/components/QuizArea.vue
+++ ver1.0.0/src/components/QuizArea.vue
@@ -1,16 +1,38 @@
 <template>
+  <button @click="refreshQuiz" id="refreshQuiz">
+    <img src="../assets/refresh-page-option.png" alt="초기화">
+  </button>
+  <button @click="back" id="backQuiz">
+    <img src="../assets/return.png" alt="뒤로가기">
+  </button>
+  <OptionsMenu @clickOption="showChoice"/>
   <table ref="table" id="Quiz-area">
-    <tr v-for="(item, index) in quizletterset" :key="item.id" :aria-rowindex="index">
-      <Letter :action="action" :rowIndex="index" v-for="(value, name) in item" :key="name.id" :colIndex="name" :letter="value"/>
+    <tr v-for="(item, index) in quizletterset"
+      :key="item.id"
+      :aria-rowindex="index">
+      <Letter
+      @clickOnLetter="updateTarget"
+      @toggleChoice="ppChoice"
+        :rowIndex="index"
+        v-for="(value, name) in item"
+        :key="name.id" :colIndex="name"
+        :letter="value.letter"
+        :isTarget="value.isTarget"
+        :isChoice="value.isChoice"
+        :isChosen="value.isChosen"
+        :isAnswer="value.isAnswer"
+        />
     </tr>
   </table>
-  <OptionsMenu/>
 </template>
 
 <script>
+import { ref } from 'vue'
 import Letter from './Letter.vue'
 import OptionsMenu from './OptionsMenu.vue'
 import quizletterset from '../assets/quizletterset.json'
+import wordlist from '../assets/wordlist.json'
+import mergelist from '../assets/valid_mergewordlist.json'
 
 export default {
     name: 'QuizArea',
@@ -18,28 +40,139 @@ export default {
     data() {
       return {
         quizletterset: quizletterset,
-        target: null,
-        chosen: [],
-        action: null,
-        pastSet: []
+        wordlist: wordlist,
+        mergelist: mergelist,
+        chosen: ref([]),
+        defaultSet: ref([]),
+        pastSet: ref([]),
+        maxChosen: ref(Number)
       }
     },
-    methods: {
+    beforeMount() {
+      // used when quizreset()
+      this.defaultSet = quizletterset
     },
-    mounted() {
-      this.emitter.on('showChoices', (data, type) => {
-        this.target = data
-        this.action = type
-      }),
-      this.emitter.on('ppChoices', (data, type) => {
-        if (type === 'push') {
-          this.chosen.push(data)
+    beforeUpdate() {
+      // used when back()
+      this.pastSet = quizletterset
+
+    },
+    methods: {
+      updateTarget(data) {
+        if (this.chosen.length == 6&&this.quizletterset[data.row][data.col].isTarget) {
+          // forceword()
+          this.word(this.chosen)
         } else {
-          this.chosen.pop(data)
+          // toggle target
+          this.quizletterset[data.row][data.col].isTarget = !this.quizletterset[data.row][data.col].isTarget
+          this.emitter.emit('toggleShow', this.quizletterset[data.row][data.col].isTarget)
+
+          if (this.quizletterset[data.row][data.col].isTarget) {
+            this.chosen.length = 0
+            this.chosen.push({'row': data.row, 'col': data.col})
+
+            for (let r=0;r<6;r++) {
+              for (let c=0;c<15;c++) {
+                if (r != data.row||c != data.col) {
+                  // reset
+                  this.quizletterset[r][c].isTarget = false
+                  this.quizletterset[r][c].isChoice = false
+                  this.quizletterset[r][c].isChosen = false
+                }
+              }
+            }
+          }
+        }
+      },
+      showChoice(data) {
+        let target = this.chosen[0]
+
+        if (data == 'merge') {
+          this.maxChosen = 2
+
+          this.quizletterset[target.row][target.col-1].isChoice = true
+          this.quizletterset[target.row][target.col+1].isChoice = true
+        } else if (data == 'word') {
+          this.maxChosen = 6
+
+          this.quizletterset[target.row][target.col+1].isChoice = true
+          this.quizletterset[target.row+1][target.col].isChoice = true
+          this.quizletterset[target.row+1][target.col+1].isChoice = true
+          this.quizletterset[target.row+2][target.col].isChoice = true
+          this.quizletterset[target.row+2][target.col+1].isChoice = true
+        } else {
+          // data === null
+          let wordspace = {'0,0':null, '1,0':null, '0,1':null, '1,1':null, '0,2':null, '1,2':null}
+
+          for (let coord in wordspace) {
+            let x = parseInt(coord.split(',')[0])
+            let y = parseInt(coord.split(',')[1])
+
+            if (coord == '1,1'||this.quizletterset[target.row+y][target.col+x].letter) {
+              wordspace[coord] = null
+            } else {
+              wordspace[coord] = this.quizletterset[target.row+y][target.col+x].letter
+            }
+          }
+
+          this.space(wordspace)
+        }
+      },
+      merge(arr) {
+        const l1 = this.quizletterset[arr[0].row][arr[0].col].letter
+        const l2 = this.quizletterset[arr[1].row][arr[1].col].letter
+
+        console.log(this.mergelist.merge[`${l1},${l2}`])
+        if (mergelist.valid[`${l1}`].includes(`${l2}`)) {
+          this.quizletterset[arr[0].row][Math.min(arr[0].col, arr[1].col)].letter = this.mergelist.merge[`${l1},${l2}`]
+          this.quizletterset[arr[0].row][Math.max(arr[0].col, arr[1].col)].letter = ''
+        } else {
+            console.log('mergeImpossible')
         }
 
-        console.log(this.chosen)
-      })
-    }
+        // reset()
+      },
+      word(arr) {
+        
+      },
+      ppChoice(data) {
+        if (data.action === 'push') {
+          // toggle isChoice/isChosen
+          this.quizletterset[data.choice.row][data.choice.col].isChosen = true
+          this.quizletterset[data.choice.row][data.choice.col].isChoice = false
+
+          this.chosen.push(data.choice)
+
+          if (this.chosen.length == this.maxChosen) {
+              // merge()
+              this.merge(this.chosen)
+          }
+        } else {
+          // data.type === 'pop'
+          // toggle isChoice/isChosen
+          this.quizletterset[data.choice.row][data.choice.col].isChosen = false
+          this.quizletterset[data.choice.row][data.choice.col].isChoice = true
+
+          const i = this.chosen.indexOf(data.choice)
+          this.chosen.splice(i,1)
+        }
+      },
+      space(wordspace) {
+        let target = this.chosen[0]
+
+        if (wordspace in Object.values(this.wordlist)) {
+          console.log('spaceimpossible')
+        } else {
+          for (let coord in wordspace) {
+            let x = parseInt(coord.split(',')[0])
+            let y = parseInt(coord.split(',')[1])
+
+            this.quizletterset[target.row+y][target.col+x].letter = ''
+          }
+        }
+      },
+      refreshQuiz() {
+      }
+  }
 }
 </script>

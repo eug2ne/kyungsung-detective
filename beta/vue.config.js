9d3984c98500342d2afd8630dccc14284bf15496
eug2ne <rebor2dlivekirax2@gmail.com>
2024-02-13 01:46:43 +0900

    ㄹㅅㅎㄴㄷㄱㅅㄴ

--- beta/vue.config.js
+++ beta/vue.config.js
@@ -1,4 +1,25 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 module.exports = {
+  configureWebpack: (config) => {
+    // get a reference to the existing ForkTsCheckerWebpackPlugin
+    const existingForkTsChecker = config.plugins.filter(
+      p => p instanceof ForkTsCheckerWebpackPlugin,
+    )[0];
+
+    // remove the existing ForkTsCheckerWebpackPlugin
+    // so that we can replace it with our modified version
+    config.plugins = config.plugins.filter(
+      p => !(p instanceof ForkTsCheckerWebpackPlugin),
+    );
+
+    // copy the options from the original ForkTsCheckerWebpackPlugin
+    // instance and add the memoryLimit property
+    const forkTsCheckerOptions = existingForkTsChecker ? existingForkTsChecker.options : { typescript: {} };
+    forkTsCheckerOptions.typescript.memoryLimit = 8192;
+
+    config.plugins.push(new ForkTsCheckerWebpackPlugin(forkTsCheckerOptions));
+  },
   chainWebpack: config => {
     /* disable insertion of assets as data urls b/c Phaser doesn't support it */
     const rules = [

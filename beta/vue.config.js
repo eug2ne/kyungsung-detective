1396d5480ec2829abadebdb0156fa87effcc363b
eug2ne <rebor2dlivekirax2@gmail.com>
2024-02-13 13:27:51 +0900

    Update vue.config.js

--- beta/vue.config.js
+++ beta/vue.config.js
@@ -1,22 +1,14 @@
 module.exports = {
   chainWebpack: config => {
     /* disable insertion of assets as data urls b/c Phaser doesn't support it */
-    const rules = [
-      { name: 'images', dir: 'img' },
-      { name: 'media',  dir: 'media' }
-    ]
-    rules.forEach(rule => {
-      const ruleConf = config.module.rule(rule.name)
-
-      ruleConf.uses.clear()
-
-      ruleConf
-        .use('file-loader')
-          .loader('file-loader')
-          .options({
-            name: `${rule.dir}/[name].[hash:8].[ext]`
-          })
-    })
+    config.module
+      .rule('images')
+      .test(/\.(png|jpg|gif|svg)$/i)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: `img/[name].[hash:8].[ext]`
+      })
   },
   devServer: {
     hot: false
